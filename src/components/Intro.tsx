c680112ce7f47879d81a21a0b1ae4d2fe4e27682
AbdellahNassim <abdallah.nassim.meridja@gmail.com>
2025-05-01 12:08:13 +0100

    Add new CV and profile picture; updated Intro component to include CV download link

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { BsArrowRight, BsDownload, BsGithub, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import useActiveSection from "@/hooks/useActiveSection.hook";
 import { useContext } from "react";
@@ -30,7 +30,7 @@ function Intro({}: Props) {
               width={300}
               height={300}
               className="rounded-full w-32 h-32 object-cover shadow-md border-[0.35rem] border-white"
-              src="/me.webp"
+              src="/nassim_square.jpg"
               alt="Nassim Profile"
               priority
               quality="100"
@@ -57,8 +57,7 @@ function Intro({}: Props) {
       >
         <span className="text-4xl sm:text-6xl font-regular">Hi,</span>{" "}
         <span className="font-bold">I am Abdellah Nassim MERIDJA</span>,
-        Computer Systems Engineer Expert in
-        AI & Blockchain.
+        Computer Systems Engineer Expert in AI & Blockchain.
       </motion.h1>
       <motion.div
         initial={{ y: 100, opacity: 0 }}
@@ -79,33 +78,43 @@ function Intro({}: Props) {
             Contact me{" "}
             <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />{" "}
           </Link>
+          <a
+            href="/Abdellah_Nassim_MERIDJA_ATS_CV_April2025.pdf"
+            target="_blank"
+            title="Download Nassim's CV"
+            rel="noreferer noopener"
+            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full"
+          >
+            <BsDownload /> Download CV
+          </a>
         </div>
 
-        <div className="flex gap-2 justify-center">
+        <div className="flex gap-2 justify-center items-center">
           <a
             href="https://www.linkedin.com/in/abdellah-nassim-meridja-433998357"
             target="_blank"
             title="Nassim's Linkedin"
             rel="noreferer noopener"
-            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full"
+            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center justify-center w-[48px] h-[48px] rounded-full"
           >
             <BsLinkedin />
           </a>
+
           <a
             href="https://github.com/AbdellahNassim"
             target="_blank"
             title="Nassim's Github"
             rel="noreferer noopener"
-            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full"
+            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center justify-center w-[48px] h-[48px] rounded-full"
           >
             <BsGithub />
           </a>
-          {/* <a
+          <a
             href="https://www.upwork.com/freelancers/~01049fd4f1e6d37279"
             target="_blank"
             title="Nassim's Upwork"
             rel="noreferer noopener"
-            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full"
+            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center justify-center w-[48px] h-[48px] rounded-full"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -118,13 +127,13 @@ function Intro({}: Props) {
                 d="M18.561 13.158c-1.102 0-2.135-.467-3.074-1.227l.228-1.076l.008-.042c.207-1.143.849-3.06 2.839-3.06a2.705 2.705 0 0 1 2.703 2.703a2.707 2.707 0 0 1-2.704 2.702zm0-8.14c-2.539 0-4.51 1.649-5.31 4.366c-1.22-1.834-2.148-4.036-2.687-5.892H7.828v7.112a2.551 2.551 0 0 1-2.547 2.548a2.55 2.55 0 0 1-2.545-2.548V3.492H0v7.112c0 2.914 2.37 5.303 5.281 5.303c2.913 0 5.283-2.389 5.283-5.303v-1.19c.529 1.107 1.182 2.229 1.974 3.221l-1.673 7.873h2.797l1.213-5.71c1.063.679 2.285 1.109 3.686 1.109c3 0 5.439-2.452 5.439-5.45c0-3-2.439-5.439-5.439-5.439z"
               />
             </svg>
-          </a> */}
+          </a>
           <a
             href="https://inflowblog.vercel.app/"
             target="_blank"
             title="Nassim's Blog - Inflow Blog"
             rel="noreferer noopener"
-            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full"
+            className="border hover:text-gray-950 border-black/10 cursor-pointer outline-none focus:scale-110 hover:scale-110 active:scale-105 transition hover:bg-gray-50 bg-white text-gray-900 p-4 flex items-center justify-center w-[48px] h-[48px] rounded-full"
           >
             <Image src="/inflow.png" width={18} height={18} alt="inflow" />
           </a>
